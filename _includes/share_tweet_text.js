91fe5f000b13c866901d86875574dcd4a4d760b2
Josh Levinger <josh@levinger.net>
2015-04-07 11:54:44 -0700

    initial commit

---
+++ _includes/share_tweet_text.js
@@ -0,0 +1,6 @@
+//
+//  WARNING! ~ If you need to use an apostrophe ==>'<==, precede it with a backslash
+//  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
+//  If you fail to do this, everything will be ruined.
+//
+TWEET_TEXT = "Tell Congress to #StopMassSpying, don\'t reauthorize Section 215 of the Patriot Act http://www.stopmassspying.com";
